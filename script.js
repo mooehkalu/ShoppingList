0366f8cdaba32d4a5043337d2e97efb8e10ebcf0
mooehkalu <mooehkalu@gmail.com>
2018-10-04 07:06:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,12 +149,12 @@ var addtocart = [];
 function changeShoppinglist(position) {
   //document.getElementById("MyList").innerHTML = shoppinglist[position];
   var arrays = shoppinglist[position];
-  arrays = arrays.split(",");
+  //arrays = arrays.split(",");
     var e1 = arrays[0];
    var e2 = arrays[1];
  var ReplacedAmount = e2.replace(/\$/g,'');
   var eitem = prompt("Please enter new item", e1);
-  var ecost = prompt("Please enter your name", ReplacedAmount);
+  //var ecost = prompt("Please enter your name", ReplacedAmount);
   shoppinglist[position] = eitem + "," + '$' + ecost;
   displayShoppinglists();
   displayShoppingCart() 
