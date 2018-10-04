fb5079924cfcd2744eecbfd03ec85091a96b0282
mooehkalu <mooehkalu@gmail.com>
2018-10-04 06:13:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -148,15 +148,15 @@ var addtocart = [];
 
 //v3.1
 function changeShoppinglist(position) {
-  //document.getElementById("MyList").innerHTML = shoppinglist[position];
+  document.getElementById("MyList").innerHTML = shoppinglist[position];
   var arrays = shoppinglist[position];
-  arrays = arrays.split(",");
+  //arrays = arrays.split(",");
     var e1 = arrays[0];
    var e2 = arrays[1];
  var ReplacedAmount = e2.replace(/\$/g,'');
   var eitem = prompt("Please enter new item", e1);
-  var ecost = prompt("Please enter your name", ReplacedAmount);
-  shoppinglist[position] = eitem + "," + '$' + ecost;
+  //var ecost = prompt("Please enter your name", ReplacedAmount);
+  shoppinglist[position] = eitem ;
   displayShoppinglists();
   displayShoppingCart();
   //v 4.0 save cookie
